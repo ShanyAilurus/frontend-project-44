a004b865f9f3378018a68f486ce6d32f30423a0e
Nataliia Selianska <natari.sev@gmail.com>
2023-04-29 20:50:07 +0200

    fix #2 after mentor remark

--- src/index.js
+++ src/index.js
@@ -1,29 +1,23 @@
 import readlineSync from 'readline-sync';
 
-const generateRoundOfAnyGame = (description, generateRound) => {
-  const counter = 3;
+export const greeting = (description) => {
   console.log('Welcome to the Brain Games!');
-  const yourName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${yourName}!`);
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
   console.log(description);
-
-  for (let i = 0; i < counter; i += 1) {
-    const [question, trueAnswer] = generateRound();
-
-    console.log(`Question: ${question}`);
-    const answer = readlineSync.question('Your answer: ');
-
-    if (trueAnswer.toString() === answer.toString()) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${trueAnswer}'.
-      \nLet's try again, ${yourName}!`);
-      break;
-    }
-    if (i + 1 === counter) {
-      console.log(`Congratulations, ${yourName}!`);
-    }
-  }
+  return userName;
 };
 
-export default generateRoundOfAnyGame;
+export const askQuestion = (question) => console.log(`Question: ${question}`);
+
+export const getAnswer = () => readlineSync.question('Your answer: ');
+
+export const isCorrectAnswer = (trueAnswer, answer) => trueAnswer === answer;
+
+export const printCorrect = () => console.log('Correct!');
+
+export const printWrong = (trueAnswer, answer, userName) => {
+  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${trueAnswer}'.\nLet's try again, ${userName}!`);
+};
+
+export const printCongratulations = (userName) => console.log(`Congratulations, ${userName}!`);
--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,4 @@
-export const generateRandomNumber = () => Math.round(Math.random() * 100);
-
-export const randomInTheRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
-export const trueAnswerWithEven = (a) => {
-  let trueAnswer;
-  if (a % 2 === 0) {
-    trueAnswer = 'yes';
-  } else {
-    trueAnswer = 'no';
-  }
-  return trueAnswer;
-};
+export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 export const randomOperator = () => {
   const operators = ['+', '-', '*'];
@@ -19,50 +7,14 @@ export const randomOperator = () => {
   return operator;
 };
 
-export const trueAnswerWithCalc = (operator, a, b) => {
-  let result;
-  switch (operator) {
-    case '+':
-      result = a + b;
-      break;
-    case '-':
-      result = a - b;
-      break;
-    default:
-      result = a * b;
-      break;
-  }
-  return `${result}`;
-};
-
-export const trueAnswerWithGCD = (num1, num2) => {
-  let a = num1;
-  let b = num2;
-  while (a !== 0 && b !== 0) {
-    if (a > b) {
-      a %= b;
-    } else {
-      b %= a;
-    }
-  }
-  return a + b;
-};
-
-export const trueAnswerWithPrime = (a) => {
-  for (let i = 2; i < a; i += 1) {
-    if (a % i === 0 || a === 1) return 'no';
-  }
-  return 'yes';
-};
-
 export const generateProgression = () => {
   // генерируем и кладём в массив первый символ
-  const firstElement = randomInTheRange(1, 100);
+  const firstElement = getRandomNumber(1, 100);
   const array = [firstElement];
   // количество элементов в массиве
-  const number = randomInTheRange(5, 10);
+  const number = getRandomNumber(5, 10);
   // генерируем шаг прогрессиии
-  const progressionStep = randomInTheRange(1, 10);
+  const progressionStep = getRandomNumber(1, 10);
   for (let i = 0; i < number - 1; i += 1) {
     let num1 = array[i];
     num1 += progressionStep;
