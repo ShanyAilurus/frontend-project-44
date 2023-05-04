81abc081f1f414504d089854c271809a65488deb
Nataliia Selianska <natari.sev@gmail.com>
2023-05-04 13:21:03 +0200

    fix index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import readlineSync from 'readline-sync';
 const generateGameRounds = (description, generateRound) => {
   const counter = 3;
   console.log('Welcome to the Brain Games!');
-  const yourName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${yourName}!`);
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
   console.log(description);
 
   for (let i = 0; i < counter; i += 1) {
@@ -17,11 +17,11 @@ const generateGameRounds = (description, generateRound) => {
       console.log('Correct!');
     } else {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${trueAnswer}'.
-      \nLet's try again, ${yourName}!`);
+      \nLet's try again, ${userName}!`);
       break;
     }
     if (i + 1 === counter) {
-      console.log(`Congratulations, ${yourName}!`);
+      console.log(`Congratulations, ${userName}!`);
     }
   }
 };
